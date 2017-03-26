77074402ad68d8facea73b7ea958b940f3f2772b
Patrick Gao <patrickgao0922@gmail.com>
2017-03-26 11:35:23 +1100

    create Account scema

--- src/models/account.js
+++ src/models/account.js
@@ -1,4 +1,13 @@
 import mongoose from "mongoose"
+import passportLocalMongoose from "passport-local-mongoose"
 
+var Schema = mongoose.Schema
 
-var schema = mongoose.Schema
+var Account = new Schema({
+	username: String,
+	passport: String
+})
+
+Account.plugin(passportLocalMongoose)
+
+export default mongoose.model("Account", Account)
